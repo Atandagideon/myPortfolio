0bcd8f8b70e0f238538f206d641a9d1b08ac97d8
Segun Ajibola <ajbl.segun@gmail.com>
2021-09-11 03:22:15 +0100

    add smooth scrolling

--- public/anime.js
+++ public/anime.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoader", () => {
- let letter = document.querySelectorAll
+// document.addEventListener("DOMContentLoader", () => {
+//  let letter = document.querySelectorAll
 
-})
+// })
